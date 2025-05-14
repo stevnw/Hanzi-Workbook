7467072607b13216fa60dd1feeee63889b03f7ec
Steven <ster-420@protonmail.com>
2025-05-14 19:08:15 +0100

    Uploads

--- pdfshitter.py
+++ pdfshitter.py
@@ -1,14 +1,20 @@
+"""
+
+This is a python script which takes in a pdf and then "shits" it out as images into a folder called "worksheets"
+
+"""
+
 from pdf2image import convert_from_path
 import os
 
-pdf_path = 'hanzi.pdf'  # Replace with your PDF
+pdf_path = 'hanzi.pdf'  # Replace with whatever you pdf is called
 output_folder = 'worksheets'
-dpi = 200  # Lower DPI to save memory
+dpi = 200  # You might need to change this ??
 fmt = 'png'
 
 os.makedirs(output_folder, exist_ok=True)
 
-# Use batch size to prevent memory blowup
+
 pages = convert_from_path(
     pdf_path,
     dpi=dpi,
@@ -18,8 +24,7 @@ pages = convert_from_path(
     thread_count=2
 )
 
-# Rename files for clarity
 for i, path in enumerate(sorted(pages)):
-    new_path = os.path.join(output_folder, f'page_{i+1}.{fmt}')
+    new_path = os.path.join(output_folder, f'page_{i+1}.{fmt}') # Names the files as "page_xx.png"
     os.rename(path, new_path)
     print(f'Saved: {new_path}')
